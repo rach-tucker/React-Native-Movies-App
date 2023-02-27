e383393139b44089e9e7e27cfb7d523c2148227e
Rachel Tucker <rach.mello@hotmail.com>
2023-02-26 23:35:31 -0600

    added .push and .popToPop to DetailsScreen buttons.

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
-const DetailsScreen = () => {
+const DetailsScreen = ( {navigation, route} ) => {
+    console.log(route);
     return (
       <View style={styles.mainView}>
         <Text>Details Screen</Text>
           <Button
             title="Go to Image"
-            onPress={() => {console.log("Go to BigImageView")}}/>
+            onPress={() => {navigation.navigate("Go to BigImageView")}}/>
+          <Button
+            title="More Details"
+            //the 'push' below forces the stack navigator to create another instance of the detail screen (creates a new "key" each time user navigates from details to details.)
+            onPress={() => {navigation.push("Details_To_Details")}}/>
+          <Button
+            title="Go Back a Screen"
+            onPress={() => {navigation.popToTop()}}/>
       </View>
     )
 };
