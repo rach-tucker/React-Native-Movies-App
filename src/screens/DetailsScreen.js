51200bbe1514f3dc5b4351590375881162d33d1c
Rachel Tucker <rach.mello@hotmail.com>
2023-02-27 21:27:33 -0600

    Accessed OMDb Web API with XMLHttpRequest

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,24 +1,46 @@
 import React, {useEffect} from 'react';
+import {useState} from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 const DetailsScreen = ( {navigation, route} ) => {
     console.log(route);
     const movie = route.params.movie;
+    const [movieDetails, setMovieDetails] = useState(null);
     useEffect(() => {
       const xhr = new XMLHttpRequest();
     //takes in 2 parameters, the method type and the URL of the request. 
       xhr.open('GET', "https://www.omdbapi.com/?apikey=92238e89&t=star+wars&y=1977");
       xhr.send();
+      xhr.onload = () => {
+        if (xhr.status == 200){
+          let response = JSON.parse(xhr.response);
+          setMovieDetails(response);
 
-      console.log(xhr.responseText);
+        }else{
+          console.log(`HTTP Request Failed ${xhr.status}`);
+        }
+      };
+      xhr.onerror = () => {
+        console.log("Error")
+      };
+
 
     }, []); //empty array indicates the function should only be called once after the screen is loaded
     
 
     
     return (
+      //values initially set to null so that when the screen is first accessed, there isn't an error since the information from the XHR has not been loaded.
       <View style={styles.mainView}>
-        
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Title)
+        }</Text>
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Released)
+        }</Text>
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Plot)
+        }</Text>
       </View>
     )
 };
